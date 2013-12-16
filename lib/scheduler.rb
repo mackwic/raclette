289716737b3629cf7ec4eb8652955aaa3f5d3f57
Thomas Wickham <mackwic@gmail.com>
2013-12-16 18:42:55 +0100

    make Scheduler synchronuous for tests

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -1,32 +1,60 @@
+require 'thread'
 require 'thread/pool'
 
 module Raclette
   class Scheduler
 
     def self.plan(&task)
-      task = task.curry[me.queue]
-      me.queue << task
+      me.queue << task.curry
     end
 
-    def shutdown
-      queue << 'exit'
-      me.pool.shutdown
+    if ENV['ENV'] == 'test'
+      def self.shutdown
+        me.queue << 'exit'
+        me.pool.shutdown
+      end
+    else
+      def self.shutdown
+        flush
+      end
     end
 
     def self.me
-      @@self ||= Scheduler.new
+      @@_self ||= Scheduler.new
     end
 
-    attr_reader :queue
+    if ENV['ENV'] == 'test'
+      def self.flush
+        while me.queue.length > 0
+          me.queue.pop.call(me.queue)
+        end
+      end
+    end
+
+    attr_reader :pool, :queue
 
     private
     def initialize
       @queue = Queue.new
+
+      # in test env, we want to use Scheduler.flush to process tasks
+      return if ENV['ENV'] == 'test'
+
       @pool = Thread.pool(100)
-      Thread.new do
-        task = @queue.pop
-        return if task == 'exit'
-        pool.process(&task)
+      Thread.new @queue do |queue|
+        loop do
+          task = queue.pop
+          break if task == 'exit'
+
+          @pool.process(task, queue) do |t, q|
+            begin
+              t.call(q)
+            rescue => e
+              puts e.message
+              p e.backtrace
+            end
+          end
+        end
       end
     end
   end
