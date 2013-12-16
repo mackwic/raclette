66e3e0979101138e6eb5ac461bef0f3c735a7fc6
Thomas Wickham <mackwic@gmail.com>
2013-12-16 18:47:35 +0100

    Test: create VoidObject and import a CountdownLatch

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,44 @@
+#intialize minitest and coveralls
+require 'minitest/autorun'
+require 'simplecov'
+require 'coveralls'
+SimpleCov.start
+Coveralls.wear!
+
 Bundler.require :default, :test
 Dir.glob('./lib/**/*.rb').each {|f| require f}
 
-#intialize minitest and coveralls
-require 'minitest/autorun'
-require 'coveralls'
-Coveralls.wear!
+class VoidObject
+  def method_missing(name, *args)
+    self
+  end
+end
 
+# from https://gist.github.com/tkareine/739662
+class CountDownLatch
+  attr_reader :count
+   
+  def initialize(to)
+    require 'thread'
+    @count = to.to_i
+    raise ArgumentError, "cannot count down from negative integer" unless @count >= 0
+    @lock = Mutex.new
+    @condition = ConditionVariable.new
+  end
+   
+  def count_down
+    @lock.synchronize do
+      @count -= 1 if @count > 0
+      @condition.broadcast if @count == 0
+    end
+  end
+   
+  def wait
+    @lock.synchronize do
+      @condition.wait(@lock) while @count > 0
+    end
+  end
+end
+
+include Raclette
 
